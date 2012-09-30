f340a04bb7cfd86835f2e387fd8d11b912a36490
xing <xing@xing-Aspire.(none)>
2012-10-01 00:55:38 +0800

    change "loggers" to only one "logger" in golog.go

--- golog.go
+++ golog.go
@@ -12,23 +12,22 @@ import (
 // log levels
 const (
 	LOGLEVEL_FATAL = iota
-	LOGLEVEL_ALERT
 	LOGLEVEL_ERROR
 	LOGLEVEL_WARN
 	LOGLEVEL_CONFIG
 	LOGLEVEL_INFO
 	LOGLEVEL_DEBUG
-	NR_LOGLEVELS
+	nr_loglevels // levels count
 )
 
 // flags
 const (
-	FLAG_Ldate = log.Ldate     // the date: 2009/01/23
-	FLAG_Ltime = log.Ltime     // the time: 01:23:23
-	FLAG_Lmicroseconds = log.Lmicroseconds  // microsecond resolution: 01:23:23.123123.  assumes Ltime.
-	FLAG_Llongfile = log.Llongfile       // full file name and line number: /a/b/c/d.go:23
-	FLAG_Lshortfile = log.Lshortfile        // final file name element and line number: d.go:23. overrides Llongfile
-	FLAG_LstdFlags = log.LstdFlags // initial values for the standard logger
+	FLAG_Ldate         = log.Ldate         // the date: 2009/01/23
+	FLAG_Ltime         = log.Ltime         // the time: 01:23:23
+	FLAG_Lmicroseconds = log.Lmicroseconds // microsecond resolution: 01:23:23.123123.  assumes Ltime.
+	FLAG_Llongfile     = log.Llongfile     // full file name and line number: /a/b/c/d.go:23
+	FLAG_Lshortfile    = log.Lshortfile    // final file name element and line number: d.go:23. overrides Llongfile
+	FLAG_LstdFlags     = log.LstdFlags     // initial values for the standard logger
 )
 
 // used to print file and line information 
@@ -36,15 +35,25 @@ const calldepth = 2
 
 type Logger struct {
 	logLevel int
-	loggers  []*log.Logger
-	prefix   string
-	writer   io.Writer
+	logger   *log.Logger
 }
 
 type NullWriter struct{}
 
 func (f *NullWriter) Write(p []byte) (int, error) {
 	return len(p), nil
+}
+
+var logLevelToName map[int]string
+
+func init() {
+	logLevelToName = make(map[int]string, nr_loglevels)
+	logLevelToName[LOGLEVEL_DEBUG] = "[Debug] "
+	logLevelToName[LOGLEVEL_INFO] = "[Info] "
+	logLevelToName[LOGLEVEL_CONFIG] = "[Config] "
+	logLevelToName[LOGLEVEL_WARN] = "[Warning] "
+	logLevelToName[LOGLEVEL_ERROR] = "[Error] "
+	logLevelToName[LOGLEVEL_FATAL] = "[Fatal] "
 }
 
 func checkErr(err error) {
@@ -54,88 +63,95 @@ func checkErr(err error) {
 }
 
 func (l *Logger) Debug(v ...interface{}) {
-	err := l.loggers[LOGLEVEL_DEBUG].Output(calldepth, fmt.Sprint(v...))
+	if l.logLevel < LOGLEVEL_DEBUG {
+		return
+	}
+	err := l.logger.Output(calldepth, logLevelToName[LOGLEVEL_DEBUG]+fmt.Sprint(v...))
 	checkErr(err)
 }
 
 func (l *Logger) Debugf(format string, v ...interface{}) {
-	err := l.loggers[LOGLEVEL_DEBUG].Output(calldepth, fmt.Sprintf(format, v...))
+	if l.logLevel < LOGLEVEL_DEBUG {
+		return
+	}
+	err := l.logger.Output(calldepth, logLevelToName[LOGLEVEL_DEBUG]+fmt.Sprintf(format, v...))
 	checkErr(err)
 }
 
 func (l *Logger) Info(v ...interface{}) {
-	err := l.loggers[LOGLEVEL_INFO].Output(calldepth, fmt.Sprint(v...))
+	if l.logLevel < LOGLEVEL_INFO {
+		return
+	}
+	err := l.logger.Output(calldepth, logLevelToName[LOGLEVEL_INFO]+fmt.Sprint(v...))
 	checkErr(err)
 }
 
 func (l *Logger) Infof(format string, v ...interface{}) {
-	err := l.loggers[LOGLEVEL_INFO].Output(calldepth, fmt.Sprintf(format, v...))
+	if l.logLevel < LOGLEVEL_INFO {
+		return
+	}
+	err := l.logger.Output(calldepth, logLevelToName[LOGLEVEL_INFO]+fmt.Sprintf(format, v...))
 	checkErr(err)
 }
 
 func (l *Logger) Config(v ...interface{}) {
-	err := l.loggers[LOGLEVEL_CONFIG].Output(calldepth, fmt.Sprint(v...))
+	if l.logLevel < LOGLEVEL_CONFIG {
+		return
+	}
+	err := l.logger.Output(calldepth, logLevelToName[LOGLEVEL_CONFIG]+fmt.Sprint(v...))
 	checkErr(err)
 }
 
 func (l *Logger) Configf(format string, v ...interface{}) {
-	err := l.loggers[LOGLEVEL_CONFIG].Output(calldepth, fmt.Sprintf(format, v...))
+	if l.logLevel < LOGLEVEL_CONFIG {
+		return
+	}
+	err := l.logger.Output(calldepth, logLevelToName[LOGLEVEL_CONFIG]+fmt.Sprintf(format, v...))
 	checkErr(err)
 }
 
 func (l *Logger) Warn(v ...interface{}) {
-	err := l.loggers[LOGLEVEL_WARN].Output(calldepth, fmt.Sprint(v...))
+	if l.logLevel < LOGLEVEL_WARN {
+		return
+	}
+	err := l.logger.Output(calldepth, logLevelToName[LOGLEVEL_WARN]+fmt.Sprint(v...))
 	checkErr(err)
 }
 
 func (l *Logger) Warnf(format string, v ...interface{}) {
-	err := l.loggers[LOGLEVEL_WARN].Output(calldepth, fmt.Sprintf(format, v...))
+	if l.logLevel < LOGLEVEL_WARN {
+		return
+	}
+	err := l.logger.Output(calldepth, logLevelToName[LOGLEVEL_WARN]+fmt.Sprintf(format, v...))
 	checkErr(err)
 }
 
 func (l *Logger) Error(v ...interface{}) {
-	err := l.loggers[LOGLEVEL_ERROR].Output(calldepth, fmt.Sprint(v...))
+	if l.logLevel < LOGLEVEL_ERROR {
+		return
+	}
+	err := l.logger.Output(calldepth, logLevelToName[LOGLEVEL_ERROR]+fmt.Sprint(v...))
 	checkErr(err)
 }
 
 func (l *Logger) Errorf(format string, v ...interface{}) {
-	err := l.loggers[LOGLEVEL_ERROR].Output(calldepth, fmt.Sprintf(format, v...))
-	checkErr(err)
-}
-
-func (l *Logger) Alert(v ...interface{}) {
-	err := l.loggers[LOGLEVEL_ALERT].Output(calldepth, fmt.Sprint(v...))
-	checkErr(err)
-}
-
-func (l *Logger) Alertf(format string, v ...interface{}) {
-	err := l.loggers[LOGLEVEL_ALERT].Output(calldepth, fmt.Sprintf(format, v...))
+	if l.logLevel < LOGLEVEL_ERROR {
+		return
+	}
+	err := l.logger.Output(calldepth, logLevelToName[LOGLEVEL_ERROR]+fmt.Sprintf(format, v...))
 	checkErr(err)
 }
 
 func (l *Logger) Fatal(v ...interface{}) {
-	err := l.loggers[LOGLEVEL_FATAL].Output(calldepth, fmt.Sprint(v...))
+	defer os.Exit(1)
+	err := l.logger.Output(calldepth, logLevelToName[LOGLEVEL_FATAL]+fmt.Sprint(v...))
 	checkErr(err)
-	os.Exit(1)
 }
 
 func (l *Logger) Fatalf(format string, v ...interface{}) {
-	err := l.loggers[LOGLEVEL_FATAL].Output(calldepth, fmt.Sprintf(format, v...))
+	defer os.Exit(1)
+	err := l.logger.Output(calldepth, logLevelToName[LOGLEVEL_FATAL]+fmt.Sprintf(format, v...))
 	checkErr(err)
-	os.Exit(1)
-}
-
-var logLevelToName map[int]string
-
-func init() {
-	logLevelToName = make(map[int]string, NR_LOGLEVELS)
-	logLevelToName[LOGLEVEL_DEBUG] = "[Debug]"
-	logLevelToName[LOGLEVEL_INFO] = "[Info]"
-	logLevelToName[LOGLEVEL_CONFIG] = "[Config]"
-	logLevelToName[LOGLEVEL_WARN] = "[Warning]"
-	logLevelToName[LOGLEVEL_ERROR] = "[Error]"
-	logLevelToName[LOGLEVEL_ALERT] = "[Alert]"
-	logLevelToName[LOGLEVEL_FATAL] = "[Fatal]"
 }
 
 // if @writer is nil, log nothing
@@ -147,29 +163,17 @@ func init() {
 // to output date and time, then golog.FLAG_Ldate | golog.FLAG_Ltime
 func NewLogger(writer io.Writer, prefix string, logLevel int, flag int) *Logger {
 	ret := new(Logger)
-	ret.loggers = make([]*log.Logger, NR_LOGLEVELS)
 	if writer == nil {
-		ret.writer = &NullWriter{}
-	} else {
-		ret.writer = writer
+		writer = &NullWriter{}
 	}
-	ret.prefix = prefix
-	ret.setLogLevel(logLevel, flag)
-	return ret
-}
 
-func (l *Logger) setLogLevel(logLevel int, flag int) {
-	if logLevel > LOGLEVEL_DEBUG {
+	if logLevel < LOGLEVEL_FATAL {
+		logLevel = LOGLEVEL_FATAL
+	} else if logLevel > LOGLEVEL_DEBUG {
 		logLevel = LOGLEVEL_DEBUG
-	} else if logLevel < LOGLEVEL_FATAL {
-		logLevel = LOGLEVEL_FATAL - 1
 	}
-	l.logLevel = logLevel
-	for i := LOGLEVEL_FATAL; i <= logLevel; i++ {
-		l.loggers[i] = log.New(l.writer, l.prefix+logLevelToName[i]+" ", flag)
-	}
-	nullwriter := &NullWriter{}
-	for i := logLevel + 1; i < NR_LOGLEVELS; i++ {
-		l.loggers[i] = log.New(nullwriter, l.prefix+logLevelToName[i]+" ", flag)
-	}
+	ret.logLevel = logLevel
+
+	ret.logger = log.New(writer, prefix+" ", flag)
+	return ret
 }
